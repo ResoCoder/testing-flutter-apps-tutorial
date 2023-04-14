f8f91f41ac81c6db79f23d1d25a0b7f8da542c51
Swayamshree Mohanty <swayamshree.appdev@colourmoon.com>
2023-04-14 11:42:00 +0530

    code refactored

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,18 @@ import 'package:flutter_testing_tutorial/news_change_notifier.dart';
 import 'package:flutter_testing_tutorial/news_page.dart';
 import 'package:flutter_testing_tutorial/news_service.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'News App',
       home: ChangeNotifierProvider(
         create: (_) => NewsChangeNotifier(NewsService()),
-        child: NewsPage(),
+        child: const NewsPage(),
       ),
     );
   }
